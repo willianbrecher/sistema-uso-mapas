0a0b34eb1408deb75e308c19fb368defca7b85d4
Willian de Souza Brecher <willian.brecher@pti.org.br>
2020-08-22 01:09:04 -0300

    Implementacao das acoes sobre o mapa

--- frontend/app.js
+++ frontend/app.js
@@ -2,14 +2,39 @@ var app = angular.module('mapApp', []);
 
 const baseUrl = 'http://localhost:3000';
 
-app.controller('SideMenuController', function($scope, $http) {
-    $http.get(baseUrl+'/coordenates').then(function successCallBack(response) {
-        console.log(response.data);
-        $scope.coordenates = response.data;
-    });
-});
-
 var map = L.map('map').setView([-25.5012, -54.5782], 15);
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 L.control.scale().addTo(map);
+
+app.controller('AppController', function ($scope, $http) {
+    $http.get(baseUrl + '/coordenates').then(function successCallBack(response) {
+        $scope.coordenates = response.data;
+    });
+
+    $scope.SubmitForm = function () {
+        // const coordenates = new FormData();
+        // coordenates.append("name", $scope.coordenate.name);
+        $http.put(baseUrl + '/coordenates/'+$scope.coordenate._id, $scope.coordenate).then(function successCallBack(response) {
+            console.log(response.data);
+        });
+    }
+
+    $scope.loadCoordenate = function (id) {
+        $http.get(baseUrl + '/coordenates/' + id).then(function successCallBack(response) {
+            $scope.coordenate = response.data;
+            console.log(response.data);
+            renderPoint($scope.coordenate);
+        });
+    }
+});
+
+function renderPoint(coordenate) {
+    L.marker([coordenate.latitude, coordenate.longitude])
+    .bindPopup("<label>"+coordenate.name+"</label>" + 
+                "<button class='btn' data-toggle='modal' data-toggle='modal' data-target='#formModal'><i class='fa fa-edit'></button>" + 
+                "")
+    .addTo(map)
+    .openPopup();
+
+    map.setView([coordenate.latitude, coordenate.longitude], 20);
+}
